ef319106f3edaa9a9f52ebdd46111fe6d1ddb5f2
Mukhammad mkhsem <adida827882@gmail.com>
2026-07-31 21:58:35 +0300

    add homework 7

---
+++ homework-7.js
@@ -0,0 +1,41 @@
+// 3.
+
+function reportsTemperature(city, temperature) {
+  console.log(`Сейчас в ${city} температура ${temperature} градусов по Цельсию`)
+}
+
+reportsTemperature('X', 'Y')
+
+
+// 4.
+
+const SPEED_LIGHT = 299792458
+
+function speedometry(speed) {
+  if (speed > SPEED_LIGHT) {
+    console.log('Сверхсветовая скорость')
+  } else if (speed < SPEED_LIGHT) {
+    console.log('Субсветовая скорость')
+  } else {
+    console.log('Скорость света')
+  }
+}
+
+speedometry()
+
+
+// 5.
+
+let product = 'Фонарь';
+let productPrice = 1000
+
+const buy = budget => {
+  if (budget > productPrice) {
+    console.log(`${product} приобретён. Спасибо за покупку!`)
+  } else {                                                              // Сделал как тз
+    let shortfall = productPrice - budget;
+    console.log(`Вам не хватает ${shortfall}$, пополните баланс`)
+  }
+}
+
+buy(1000)
